61b9de0138a224b13d2917a28dedbf80646112aa
LakeSuburbia <74195406+LakeSuburbia@users.noreply.github.com>
2021-02-01 11:03:13 +0100

    Update Temperatures.rb

--- Temperatures.rb
+++ Temperatures.rb
@@ -1,5 +1,3 @@
 gets.to_i==0?x=0:x=5529
-for c in gets.split(" ")
-t=c.to_i
-(t.abs<x.abs)?(x=t):(t.abs==x.abs and t>x)?(x=t):c end
+gets.split(" ").each{|c|t=c.to_i;(t.abs<x.abs||t.abs==x.abs&&t>x)?(x=t):c}
 puts x
