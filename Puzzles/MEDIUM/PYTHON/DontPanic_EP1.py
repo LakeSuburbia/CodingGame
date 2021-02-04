964b38fc8a4116da6e4fede52c2c40f5c16a304f
LakeSuburbia <74195406+LakeSuburbia@users.noreply.github.com>
2021-02-04 13:57:05 +0100

    Update DontPanic_EP1.py

--- Puzzles/MEDIUM/PYTHON/DontPanic_EP1.py
+++ Puzzles/MEDIUM/PYTHON/DontPanic_EP1.py
@@ -1,10 +1,32 @@
-# garbage variable names due to codegolf
-e,v,W,B={},[],"WAIT",'BLOCK'
-o,l,u,z,s,t,a,r=[int(i) for i in input().split()]
-for i in range(r):v.append([int(j) for j in input().split()]);e[v[i][0]]=v[i][1]
+elevator_pos, elevator_input, WAIT, BLOCK = {}, [], "WAIT", "BLOCK"
+(
+    nb_floors,
+    width,
+    nb_rounds,
+    exit_floor,
+    exit_pos,
+    nb_total_clones,
+    nb_additional_elevators,
+    nb_elevators,
+) = [int(i) for i in input().split()]
+
+for i in range(nb_elevators):
+    elevator_input.append([int(j) for j in input().split()])
+    elevator_pos[elevator_input[i][0]] = elevator_input[i][1]
+
 while 1:
- f,p,d=input().split()
- f,p=int(f),int(p)
- if f==z or f==-1:y=(W,B)[(p>s and d[:1]=='R')or(p<s and d[:1]=='L')]
- else:y=(W,B)[(p>e[f] and d[:1]=='R')or(p<e[f] and d[:1]=='L')]
- print(y)
+    clone_floor, clone_pos, direction = input().split()
+    clone_floor, clone_pos = int(clone_floor), int(clone_pos)
+
+    if clone_floor == exit_floor or clone_floor == -1:
+        command = (WAIT, BLOCK)[
+            (clone_pos > exit_pos and direction[:1] == "R")
+            or (clone_pos < exit_pos and direction[:1] == "L")
+        ]
+    else:
+        command = (WAIT, BLOCK)[
+            (clone_pos > elevator_pos[clone_floor] and direction[:1] == "R")
+            or (clone_pos < elevator_pos[clone_floor] and direction[:1] == "L")
+        ]
+        
+    print(command)
