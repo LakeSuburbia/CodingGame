e634dfd993f2bd7884a28f7cc2e3dc6032e799f3
LakeSuburbia <sandermangelschots@telenet.be>
2021-05-19 23:20:04 +0200

    Optimize Dont Panic

--- Code Golf/DontPanic.py
+++ Code Golf/DontPanic.py
@@ -1,9 +1,5 @@
-e,v,W,B={},[],"WAIT",'BLOCK'
-o,l,u,z,s,t,a,r=[int(i) for i in input().split()]
-for i in range(r):v.append([int(j) for j in input().split()]);e[v[i][0]]=v[i][1]
-while 1:
- f,p,d=input().split()
- f,p=int(f),int(p)
- if f==z or f==-1:y=(W,B)[(p>s and d[:1]=='R')or(p<s and d[:1]=='L')]
- else:y=(W,B)[(p>e[f] and d[:1]=='R')or(p<e[f] and d[:1]=='L')]
- print(y)
+a,q,i=input,int,lambda:map(q,a().split())
+_,_,_,d,e,_,_,n=i()
+z={d:e}
+for _ in[1]*n:j,k=i();z[j]=k
+while 1:l,m,o=a().split();o,l,m=len(o),q(l),q(m);print(["WAIT","BLOCK"][l>-1 and(m>z[l]>0<o>4 or m<z[l]>0<o<5)])
