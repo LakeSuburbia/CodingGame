e4ff9cccc841bd7c1f405af934b44d366d18e1eb
LakeSuburbia <sandermangelschots@telenet.be>
2021-05-19 23:20:31 +0200

    Optimize Dont Panic (208 char)

--- Code Golf/DontPanic.py
+++ Code Golf/DontPanic.py
@@ -1,5 +1,5 @@
-a,q,i=input,int,lambda:map(q,a().split())
+i=lambda:map(int,input().split())
 _,_,_,d,e,_,_,n=i()
 z={d:e}
-for _ in[1]*n:j,k=i();z[j]=k
-while 1:l,m,o=a().split();o,l,m=len(o),q(l),q(m);print(["WAIT","BLOCK"][l>-1 and(m>z[l]>0<o>4 or m<z[l]>0<o<5)])
+exec("j,k=i();z[j]=k;"*n)
+while 1:l,m,o=input().split();o,l,m=len(o),int(l),int(m);print(["WAIT","BLOCK"][l>-1 and(m>z[l]>0<o>4 or m<z[l]>0<o<5)])
