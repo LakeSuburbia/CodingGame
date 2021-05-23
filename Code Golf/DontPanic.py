8929a93d345c8b60b646da7da89119536198b22f
LakeSuburbia <sandermangelschots@telenet.be>
2021-05-23 22:24:52 +0200

    Code golfed to the max

--- Code Golf/DontPanic.py
+++ Code Golf/DontPanic.py
@@ -1,5 +1,4 @@
-i=lambda:map(int,input().split())
-_,_,_,d,e,_,_,n=i()
-z={d:e}
-exec("j,k=i();z[j]=k;"*n)
-while 1:l,m,o=input().split();o,l,m=len(o),int(l),int(m);print(["WAIT","BLOCK"][l>-1 and(m>z[l]>0<o>4 or m<z[l]>0<o<5)])
+a,b,i=input,int,lambda:map(b,a().split())
+*c,d,e,_,_,n=i()
+z={d:e};exec("j,k=i();z[j]=k;"*n)
+while 1:l,m,o=a().split();o,l,m=len(o),b(l),b(m);print(["WAIT","BLOCK"][l>-1 and(o>4<m>z[l]or m<z[l]>o<5)])
